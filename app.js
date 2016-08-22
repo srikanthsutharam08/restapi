3aeceba5ab6dbfd7078fe6e25a944c92316138a6
srikanth-sutharam <srikanth.sutharam@peel.com>
2016-08-22 17:19:19 +0530

    added email

--- app.js
+++ app.js
@@ -130,12 +130,15 @@ bot.dialog('/profileInfo', [
 	}, 
     function (session, results) {
 		profileInfo["maritalstatus"] = results.response;
+		builder.Prompts.text(session, "Please enter your email id?");
+	},
+	function (session, results) {
+		profileInfo["email"] = results.response;
 		builder.Prompts.text(session, "What is your current residing city?");
 	},
 	function (session, results) {
 		profileInfo["city"] = results.response; 
 		saveUserInfo(profileInfo)
-		//deleteUserInfo(profileInfo["user_id"])
 		//saveProfileInfo();
 		session.endDialog(JSON.stringify(profileInfo));
 	}
@@ -143,7 +146,7 @@ bot.dialog('/profileInfo', [
 
 //Save userinfo in SQL DB
 function saveUserInfo(profileInfo) {
- 	var post = {user_id:profileInfo["user_id"], user_name:profileInfo["name"], age:profileInfo["age"], gender:profileInfo["gender"], maritalstatus:profileInfo["maritalstatus"], city:profileInfo["city"]};	
+ 	var post = {user_id:profileInfo["user_id"], user_name:profileInfo["name"], age:profileInfo["age"], gender:profileInfo["gender"], email:profileInfo["email"], maritalstatus:profileInfo["maritalstatus"], city:profileInfo["city"]};	
  	var query = connection.query('INSERT INTO userinfo SET ?', post, function(err, result) {
  		if (err) 
  			throw err;
