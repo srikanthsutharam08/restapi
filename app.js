949b5c745117d6080280e9006362d9553066a38e
srikanth-sutharam <srikanth.sutharam@peel.com>
2016-08-25 12:36:29 +0530

    displaying req

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ server.post('/pushsurvey', function respond(req, res, next) {
 			bot.beginDialog(filteredUsers[key].address, '/notify');
 		}
 	}
- 	res.send('Sent survey requests to end users::'+req.body);
+ 	res.send('Sent survey requests to end users::'+JSON.stringify(req));
 })
 
 //=========================================================
