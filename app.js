a41fec24db4a028212cfc2a9c352649cb3cc3593
srikanth-sutharam <srikanth.sutharam@peel.com>
2016-08-25 11:06:20 +0530

    Call root dialog from add to contacts

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ bot.on('contactRelationUpdate', function (message) {
 		var reply = new builder.Message()
                 .address(message.address)
                 .text("Hello %s... Thanks for adding me into your contacts.Say something.", name || 'there');
-        bot.send(reply);
+        //bot.send(reply);
 		bot.beginDialog(message.user.address, '/')
     } else {
         //deleteProfileInfo(message.user.id)
